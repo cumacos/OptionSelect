cc98acfdfec0eff0388b6419f8c53a04019e5407
Ernesto Canquiz <fdsoil123@gmail.com>
2017-06-26 10:28:20 -0400

    first commit

---
+++ inicio.js
@@ -0,0 +1,12 @@
+function inicio()
+{
+
+    var arr = [ 
+                  ["1", "ARAGUA"], 
+                  ["2", "CARABOBO"], 
+                  ["3", "MIRANDA"]
+              ];
+	
+    fillCombo( document.getElementById('id_select'), arr, '0', 'Seleccione...' );
+
+}
