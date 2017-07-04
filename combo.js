f6dac65380ca168eab72f79f132e207ec50d55c9
Ernesto Canquiz <fdsoil123@gmail.com>
2017-07-04 09:45:25 -0400

    Actualizando

--- combo.js
+++ combo.js
@@ -1,28 +1,28 @@
 function fillCombo( obj, arr, valor, texto )
 {
     //vaciar combo...
-    while( obj.options.length > 0 )
-        obj.options[obj.options.length-1] = null;
+    while ( obj.options.length > 0 )
+        obj.options[ obj.options.length - 1 ] = null;
     //crear opcion por defecto (n/a para select múltiple)...
     if ( valor != null && texto != null ) {
-        var opcion = document.createElement("option");
+        var opcion = document.createElement( "option" );
         opcion.value = valor;
         opcion.text = texto;
         obj.options.add(opcion);
     }
     //crear grupo de opciones...
     var aOptions = [];
-    var oOptGroup = document.createElement('optgroup');
+    var oOptGroup = document.createElement( "optgroup" );
     for ( var i = 0; i < (arr.length); i++ ) {
-        aOptions[i] = document.createElement("option");	
+        aOptions[i] = document.createElement( "option" );
         aOptions[i].value = arr[i][0];
         aOptions[i].text = arr[i][1];
         //Para opciones con etiquetas...
         if ( arr[i][2] != undefined )
-            aOptions[i].label = arr[i][2];        
+            aOptions[i].label = arr[i][2];
         oOptGroup.appendChild( aOptions[i] );
     }	
     //colocar grupo de opciones en el elemento select...
-    obj.innerHTML += oOptGroup.innerHTML; 
+    obj.innerHTML += oOptGroup.innerHTML;
 }
 
--- inicio.js
+++ inicio.js
@@ -1,12 +1,13 @@
 function inicio()
 {
 
-    var arr = [ 
-                  ["1", "ARAGUA"], 
-                  ["2", "CARABOBO"], 
-                  ["3", "MIRANDA"]
-              ];
-	
-    fillCombo( document.getElementById('id_select'), arr, '0', 'Seleccione...' );
+    var arr = [
+       [ "1", "ARAGUA"   ],
+       [ "2", "CARABOBO" ],
+       [ "3", "MIRANDA"  ]
+    ];
+
+    fillCombo( document.getElementById( "id_select" ), arr, "0", "Seleccione..." );
 
 }
+
